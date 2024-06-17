ca51e9bffdfbbebf905a2659e69803df6e48f10c
martinaliporata <martinaliporata@gmail.com>
2024-06-17 09:33:47 +0200

    fixed problems

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -11,7 +11,48 @@
             }
         },      
         methods: {
-            
+            getCharacters(){
+                axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=40&offset=0')
+                .then((response) => {
+                    this.characters=response.data.data;
+                })
+                .catch(function(error){
+                    console.log(error);
+                })
+                .finally(function(){
+                });
+            },
+            getArchetypes(){
+                axios.get('https://db.ygoprodeck.com/api/v7/archetypes.php')
+                .then((response) => {
+                    this.archetypes=response.data;
+                })
+                .catch(function(error){
+                    console.log(error);
+                })
+                .finally(function(){
+                });
+            },
+            handleChange (event) {
+                console.log(event)
+                this.selectedArchetype=event.target.value;
+                // event.target.value è valore che seleziona utente nella select
+                axios.get(this.selectedArchetype===''?'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=40&offset=0': `https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=${this.selectedArchetype}`)
+                .then((response) => {
+                    this.characters=response.data.data;
+                    console.log('gaurda',response)
+                })
+                .catch(function(error){
+                    console.log(error);
+                })
+                .finally(function(){
+                });
+            }
+        },
+        created () {
+            this.getCharacters();
+            console.log(this.getCharacters)
+            this.getArchetypes();
         }
     }
 </script>
--- src/components/ListCards.vue
+++ src/components/ListCards.vue
@@ -7,6 +7,37 @@
         },
         components : {
             SingleCard
+        },
+        props:{
+            characters: {
+                type: Array,
+                required: true
+            }
         }
     }
 </script>
+
+<template>
+    <section class="characters-container">
+        <div class="row">
+            <div class="col-12">
+                <div class="row">
+                    <article v-for="(character,index) in characters" :key="index" class="col-3">
+                        <div class="card w-100">
+                            <img :src="character.card_images[0].image_url" alt="">
+                            <h4>
+                                {{character.name }}
+                            </h4>
+                            <p>
+                                {{character.archetype }}
+                            </p>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </div>
+    </section>
+    <SingleCard/>
+</template>
+
+<style lang="scss" scoped>
